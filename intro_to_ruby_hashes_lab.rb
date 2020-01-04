f9002be2e8bf13f85a2de13e11675e44c4207b8c
Anthony Holder <anthonyh202x@gmail.com>
2020-01-04 05:16:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,6 +27,7 @@ monopoly =
     :names => 
       {:reading_railroad => 
         #{"mortgage_value" => '$' + 100}, 
+        ,
       :pennsylvania_railroad => 
         {}, 
       :b_and_o_railroad => 
