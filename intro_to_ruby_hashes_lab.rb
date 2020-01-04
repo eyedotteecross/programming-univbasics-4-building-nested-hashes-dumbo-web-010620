6804643a292de9046eeb5a993df4be59d64b3d25
Anthony Holder <anthonyh202x@gmail.com>
2020-01-04 04:45:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,6 +13,9 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-    monopoly_with_second_tier
-    monopoly = {}
+  monopoly = {
+    :railroads => {
+      :pieces => 4
+    }
+  }
 end
